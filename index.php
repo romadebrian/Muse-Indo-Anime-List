efdc91637f6291a26f3ab93a4b4e595bac0c2c23
romadebrian <roma_coll04@yahoo.com>
2021-07-03 21:04:23 +0700

    Frist Commit

---
+++ index.php
@@ -0,0 +1,212 @@
+<html>
+<head>
+    <title>List Anime Muse Indonesia</title>
+	
+	<link href="bootstrap\css\bootstrap.min.css" rel="stylesheet">
+	
+	<link rel="icon" type="image/png" href="resource/favicon.png" />
+	
+    <!-- Custom styles for this template -->
+    <link href="headers.css" rel="stylesheet">
+	<link href="custom_style.css" rel="stylesheet">
+	
+	<style>
+	.table_border_only_header td
+	{
+		border: 0;
+	}
+	</style>
+  </head>
+
+<body>
+
+<main>
+<div class="container">
+	<nav class="py-2 bg-light border-bottom">
+    <div class="container d-flex flex-wrap">
+      <ul class="nav me-auto">
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-2 active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Anime List </a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Youtube Channel</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Facebook</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">About</a></li>
+      </ul>
+      <ul class="nav">
+        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+  <header class="py-3 mb-4 border-bottom">
+    <div class="container d-flex flex-wrap justify-content-center">
+      <a href="/index.php" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+        <!-- <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg> -->
+		<img src="resource/Logo.png" alt="Logo" width="35" height="35" class="bi me-2">
+        <span class="fs-4">ANIME LIST</span>
+      </a>
+      <form class="col-12 col-lg-auto mb-3 mb-lg-0">
+        <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+      </form>
+    </div>
+  </header>
+
+	<div class="bd-example">
+        <nav aria-label="Standard pagination example">
+          <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#List_Anime_A">A</a></li>
+            <li class="page-item"><a class="page-link" href="#List_Anime_B">B</a></li>
+            <li class="page-item"><a class="page-link" href="#List_Anime_C">C</a></li>
+			<li class="page-item"><a class="page-link" href="#List_Anime_D">D</a></li>
+            <li class="page-item"><a class="page-link" href="#">E</a></li>
+            <li class="page-item"><a class="page-link" href="#">F</a></li>
+			<li class="page-item"><a class="page-link" href="#">G</a></li>
+            <li class="page-item"><a class="page-link" href="#">H</a></li>
+            <li class="page-item"><a class="page-link" href="#">I</a></li>
+			<li class="page-item"><a class="page-link" href="#">J</a></li>
+            <li class="page-item"><a class="page-link" href="#">K</a></li>
+            <li class="page-item"><a class="page-link" href="#">L</a></li>
+			<li class="page-item"><a class="page-link" href="#">M</a></li>
+            <li class="page-item"><a class="page-link" href="#">N</a></li>
+            <li class="page-item"><a class="page-link" href="#">O</a></li>
+			<li class="page-item"><a class="page-link" href="#">P</a></li>
+            <li class="page-item"><a class="page-link" href="#">Q</a></li>
+            <li class="page-item"><a class="page-link" href="#">R</a></li>
+			<li class="page-item"><a class="page-link" href="#">S</a></li>
+            <li class="page-item"><a class="page-link" href="#">T</a></li>
+            <li class="page-item"><a class="page-link" href="#">U</a></li>
+			<li class="page-item"><a class="page-link" href="#">V</a></li>
+            <li class="page-item"><a class="page-link" href="#">W</a></li>
+            <li class="page-item"><a class="page-link" href="#">X</a></li>
+			<li class="page-item"><a class="page-link" href="#">Y</a></li>
+            <li class="page-item"><a class="page-link" href="#">Z</a></li>
+          </ul>
+        </nav>
+	</div>
+	
+	<section id="List_Anime_A">
+	<div class="bd-example">
+        <table class="table table_border_only_header">
+          <thead>
+          <tr>
+            <th scope="col">A</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZiIzcp3K5AAomp3ugF18VB"><b>A3!</b></a></td>
+          </tr>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZEmovyH76p5rW4_-D2cGK0"><b>A Certain Magical Index</b></a></td>
+          </tr>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZiIzcp3K5AAomp3ugF18VB"><b>A Certain Magical Index S2</b></a></td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+          </tbody>
+        </table>
+	</div>
+	</section>
+	
+	<section id="List_Anime_B">
+	<div class="bd-example">
+        <table class="table table_border_only_header">
+          <thead>
+          <tr>
+            <th scope="col">A</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZiIzcp3K5AAomp3ugF18VB"><b>A3!</b></a></td>
+          </tr>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZEmovyH76p5rW4_-D2cGK0"><b>A Certain Magical Index</b></a></td>
+          </tr>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZiIzcp3K5AAomp3ugF18VB"><b>A Certain Magical Index S2</b></a></td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+          </tbody>
+        </table>
+	</div>
+	</section>
+	
+	<section id="List_Anime_D">
+	<div class="bd-example">
+        <table class="table table_border_only_header">
+          <thead>
+          <tr>
+            <th scope="col">A</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZiIzcp3K5AAomp3ugF18VB"><b>A3!</b></a></td>
+          </tr>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZEmovyH76p5rW4_-D2cGK0"><b>A Certain Magical Index</b></a></td>
+          </tr>
+          <tr>
+            <td><a href="https://www.youtube.com/playlist?list=PLPanbgyToztZiIzcp3K5AAomp3ugF18VB"><b>A Certain Magical Index S2</b></a></td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+		  <tr>
+            <td>3</td>
+          </tr>
+          </tbody>
+        </table>
+	</div>
+	</section>
+
+</main>
+<div id="footer">
+</div>
+
+</html>
